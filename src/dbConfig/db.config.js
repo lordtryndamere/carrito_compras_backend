fb4721653a31ae1b95d6599c8aafcd672f5e933a
unknown <kevin.feo@educar.com.co>
2020-09-17 19:13:47 -0500

    testeado

--- src/dbConfig/db.config.js
+++ src/dbConfig/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST:"localhost",
-    USER:"root",
-    PASSWORD:"Madara*20",
-    DB:"CARRITO_DE_COMPRAS",
+    HOST:"nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    USER:"avsu9hnzdz8csje1",
+    PASSWORD:"mjkh7xra7wutcqts",
+    DB:"gpdgssb4ql7wafyr",
     dialect:"mysql",
     pool:{
         max:5,
